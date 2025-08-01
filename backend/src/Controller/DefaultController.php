d80c7b89a4dff7eb68bdf6c943ca9d91047fd73e
Jesús Gómez Beltrán <jgomezbeltran88@gmail.com>
2025-08-02 00:30:39 +0200

    Evitar bucle de redirección entre Apache y Symfony

--- backend/src/Controller/DefaultController.php
+++ backend/src/Controller/DefaultController.php
@@ -1,18 +1,18 @@
 <?php
 
 // src/Controller/DefaultController.php
-namespace App\Controller;
+//namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
-
-class DefaultController extends AbstractController
-{
-    #[Route('/', name: 'app_home')]
-    public function index(): RedirectResponse
-    {
-        return $this->redirect('/landing');
-    }
-}
-
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\RedirectResponse;
+//use Symfony\Component\Routing\Annotation\Route;
+//
+//class DefaultController extends AbstractController
+//{
+//    #[Route('/', name: 'app_home')]
+//    public function index(): RedirectResponse
+//    {
+//        return $this->redirect('/landing');
+//    }
+//}
+//
